c118282c75a30700dac71f987271f37b5544a2d5
NuremirKadyrbekov <118036760+NuremirKadyrbekov@users.noreply.github.com>
2023-01-31 23:28:46 +0600

    Add files via upload

---
+++ new-profile.js
@@ -0,0 +1,2 @@
+politics.onclick=a=>submit.style.color=a.target.checked?"#000":"#999";
+newRegistr.onclick=a=>politics.checked||(alert( "Поставьте галочку в поле о согласии обработки персональных данных"),false);
